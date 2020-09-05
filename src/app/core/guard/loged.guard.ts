ecf8c1aca6e06756cabc4d75a6d58ad6c30d015e
marina556 <marinamakram556@gmail.com>
2020-09-05 21:07:06 +0200

    add guard to login #2

---
+++ src/app/core/guard/loged.guard.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogedGuard implements CanActivate {
+  constructor(private authService: AuthService, private route: Router) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.isLogin){
+      return true;
+    }
+    else { return false; }
+      }
+
+}
